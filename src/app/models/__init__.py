4185080a346ded86dd5a0d6da61d650df3981eaa
Timur Bikmaev <bikma2004@gmail.com>
2026-04-15 21:12:17 +0300

    fresh_develop_2

--- src/app/models/__init__.py
+++ src/app/models/__init__.py
@@ -1,10 +1,5 @@
 """Пакет моделей."""
 
-<<<<<<< HEAD
-from .cafe import Cafe  # noqa
 from .user import User  # noqa
-=======
-from .user import User # noqa
-from .table import Table # noqa
-from .cafe import Cafe # noqa
->>>>>>> 5ab5e81b49c930b97a162558f9b682b7a3f1843a
+from .table import Table  # noqa
+from .cafe import Cafe  # noqa
