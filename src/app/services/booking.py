2b9105b37be4ec9db7f0765de1fdfb7a804030cc
Maxim Datskovskiy <nspmax@ya.ru>
2026-04-27 17:22:49 +0300

    fix

--- src/app/services/booking.py
+++ src/app/services/booking.py
@@ -1,28 +1,13 @@
 """Сервис для работы с бронированиями."""
 
-from app.schemas.booking import BookingCreate, BookingUpdate
-from app.models.booking import Booking
 from src.app.api.dependencies import SessionDep
 
 
-
+# TODO: Вероятно стоит перенести сюда часть работы с бронированиями
+#  (из endpoints) и добавить ворнинг на guest_number
 class BookingService:
     """Сервис для работы с бронированиями."""
 
-    def __init__(self, db: SessionDep):
+    def __init__(self, db: SessionDep) -> None:
+        """Инициализация сервиса с подключением к базе данных."""
         self.db = db
-
-    def create_booking(self, booking: BookingCreate) -> Booking:
-        pass
-
-    def update_booking(self, booking_id: int, booking: BookingUpdate) -> Booking:
-        pass
-
-    def get_booking(self, booking_id: int) -> Booking:
-        pass
-
-    def get_bookings(self) -> list[Booking]:
-        pass
-
-    def delete_booking(self, booking_id: int) -> None:
-        pass
