9f9827729cffc2de47fb8ff6913ba0a64c5ea612
Sofia Tseitlin <sofi.tseitlin@yandex.com>
2026-04-30 01:01:04 +0300

    Приводим к единому стилю по ruff

--- src/app/models/user.py
+++ src/app/models/user.py
@@ -1,5 +1,5 @@
 from enum import StrEnum
-from typing import TYPE_CHECKING, Optional
+from typing import TYPE_CHECKING
 
 from sqlalchemy import CheckConstraint, Enum, ForeignKey, String
 from sqlalchemy.orm import Mapped, mapped_column, relationship, validates
