37912e686891987c6900849039969daa6fc0b4db
Maxim Datskovskiy <nspmax@ya.ru>
2026-05-04 05:40:11 +0300

    code fix-3

--- src/app/api/validators/booking.py
+++ src/app/api/validators/booking.py
@@ -73,7 +73,7 @@ async def validate_cafe_slot_table(
         slot_tuple = tuple(sorted(table_slot.items()))
         if slot_tuple in unique_set:
             raise HTTPException(
-                status_code=status.HTTP_400_BAD_REQUEST,
+                status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
                 detail=Constants.DUBLICATE_SLOTS,
             )
         unique_set.add(slot_tuple)
@@ -82,7 +82,7 @@ async def validate_cafe_slot_table(
         or len(slot_ids) != len(slot_ids_db)
     ):
         raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
             detail=Constants.SLOTS_OR_TABLES_NOT_IN_CAFE,
         )
 
