dde056355eb8e99732b834045aa5bb6bbca68104
Daniil Klimov <x.kek97@mail.ru>
2026-05-04 17:13:14 +0300

    fix ruff

--- src/app/api/endpoints/user.py
+++ src/app/api/endpoints/user.py
@@ -231,5 +231,5 @@ async def update_user(
     except ValueError as e:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
-            detail=str(e)
+            detail=str(e),
         )
